c7a9c7be90604ce8fdbbcf7643c8211b8ae8dc7d
Codershreya2 <shreyapatel3269@gmail.com>
2026-02-03 21:02:26 +0530

    Update main.py

--- voice-api/main.py
+++ voice-api/main.py
@@ -6,29 +6,18 @@ app = FastAPI()
 API_KEY = "test123"
 
 class VoiceRequest(BaseModel):
-   {
-  "language": "English",
-  "audio_format": "mp3",
-  "audio_base64": "dummy"
-}
-
+    language: str
+    audioFormat: str
+    audioBase64: str
 
 @app.post("/detect")
 def detect_voice(data: VoiceRequest, x_api_key: str = Header(None)):
     if x_api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Invalid API Key")
 
-    {
-  "language": "english",
-  "audio_format": "mp3",
-  "audio_base64": "dummy"
-}
-
-
     return {
         "classification": "AI Generated",
         "confidence": 0.85,
         "explanation": "Voice characteristics indicate synthetic patterns"
     }
-
 
